54cb924d924628556e9d0e07946382dbe2e872fc
stephanie <thesqrtminus1@gmail.com>
2021-01-04 05:23:06 -0500

    more cleanup

--- tf.bat
+++ tf.bat
@@ -1,9 +1,20 @@
+:: goto cursor lock directory
 cd /D "C:\Program Files (x86)\Cursor Lock\"
+:: kill any running cursorlock
 taskkill -im CursorLock* /f
+:: start new cursorlock
 start CursorLock.exe /A:"C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\hl2.exe" /M:2 &
-taskkill -im vibrance* /f
+:: kill any running vibrance
+taskkill -im vibrance* /f.
+:: start vibrance
 start D:/vibrance &
+:: goto custom folder
+cd /D "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf\custom"
+:: delete sound caches on start
+del /S *.cache
+:: go to main tf2 directory
 cd /D "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\"
+:: kill any running tf2
 taskkill -im hl2.exe* /f
 ::================================
 :: clean:
@@ -23,4 +34,5 @@ taskkill -im hl2.exe* /f
 ::================================
 :: normal
 ::
+:: start the game
 start hl2.exe -game tf -steam -novid -nojoy -nosteamcontroller -particles 1 -precachefontchars -console -nostartupsound +clientport 27069 -no_texture_stream &
