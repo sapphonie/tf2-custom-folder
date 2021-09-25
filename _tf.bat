90f71ae957e41ea4d9d0d3500a24b348949312e3
stephanie <thesqrtminus1@gmail.com>
2021-09-25 19:37:29 -0400

    update 9/25/21

--- _tf.bat
+++ _tf.bat
@@ -19,20 +19,20 @@ taskkill -im hl2.exe* /f
 ::================================
 :: clean:
 ::
-:: start hl2.exe -game tf -steam -secure -w 1920 -h 1080 -fullscreen -high -novid -default -autoconfig +host_writeconfig +mat_savechanges +quit &
+:: start hl2.exe -game tf -steam -secure -w 1920 -h 1080 -fullscreen -high -novid -default -autoconfig +host_writeconfig +mat_savechanges -hushsteam +quit &
 ::================================
 :: mapping:
 ::
 :: -dxlevel 100
-:: start hl2.exe -game tf -steam -secure -w 1920 -h 1080 -fullscreen -high -novid -nojoy -primarysound -console -nostartupsound -dxlevel 100 &
+:: start hl2.exe -game tf -steam -secure -w 1920 -h 1080 -fullscreen -high -novid -nojoy -primarysound -console -nostartupsound -dxlevel 100 -hushsteam &
 ::
 ::================================
 :: first start
 ::
-:: start hl2.exe -game tf -steam -w 1920 -h 1080 -fullscreen -dxlevel 81 -high -novid -nojoy -nosteamcontroller -softparticlesdefaultoff -reuse -particles 1 -primarysound -snoforceformat -precachefontchars -console -nostartupsound +clientport 27069 -secure
+:: start hl2.exe -game tf -steam -w 1920 -h 1080 -fullscreen -dxlevel 81 -high -novid -nojoy -nosteamcontroller -softparticlesdefaultoff -reuse -particles 1 -primarysound -snoforceformat -precachefontchars -console -nostartupsound +clientport 27069 -hushsteam -secure
 ::
 ::================================
 :: normal
 ::
 :: start the game
-start hl2.exe -game tf -secure -steam -novid -nojoy -nosteamcontroller -particles 1 -precachefontchars -console -nostartupsound +clientport 27069 &
+start hl2.exe -game tf -secure -steam -novid -nojoy -nosteamcontroller -particles 1 -precachefontchars -console -nostartupsound +clientport 27069 -hushsteam &
